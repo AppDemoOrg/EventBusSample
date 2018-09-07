0c85b82347b73e6cba5e4d5b0785c24dd288fa1c
WeiQi <askviky2010@gmail.com>
2018-09-07 11:23:52 +0800

    Update MainActivity.java

--- app/src/main/java/com/abt/bus/MainActivity.java
+++ app/src/main/java/com/abt/bus/MainActivity.java
@@ -8,6 +8,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EventBus.getDefault().register(this);        
         setContentView(R.layout.activity_main);
+    }
+    
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onConnectAction(ConnectEvent event) {
+        LogHelper.d(TAG, "onConnectAction, status="+event.getStatus());
+        if (event.getStatus()) {
+            subscribeTopic();
+            publish();
+        }
+    }
+    
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
     }
 }
